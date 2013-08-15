4c44aa4a44257e70fe5271be2c23a84927402178
Wheezy123 <JohnsonJeromeC@gmail.com>
2013-08-14 21:31:09 -0500

    Update OOP_Blackjack.rb

--- OOP_Blackjack.rb
+++ OOP_Blackjack.rb
@@ -9,44 +9,44 @@
 #dealer(deal,hit,stay)
 
 class Card
-	def initialize(value,suit)
-		@value = value
-		@suit = suit
-	end
+  def initialize(value,suit)
+    @value = value
+    @suit = suit
+  end
 end
 
 class Deck(cards)
 
-	def shuffle
-	end
+  def shuffle
+  end
 
 end
 
 
 class Player
 
-	def hit
-	end
+  def hit
+  end
 
-	def stay
-	end
+  def stay
+  end
 
-	def bet
-	end
+  def bet
+  end
 
 end
 
 
 class Dealer
 
-	def deal
-	end
+  def deal
+  end
 
-	def hit
-	end
+  def hit
+  end
 
-	def stay
-	end
+  def stay
+  end
 
 end
 
